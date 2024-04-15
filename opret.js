38c61e3f7e9ca038475d85d364b0f2662771a789
Loulyb <louisejanum@hotmail.com>
2024-04-15 13:38:29 +0200

    Opret siden

---
+++ opret.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myfade");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active"); // Fjern aktiv klassen fra alle slides
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
