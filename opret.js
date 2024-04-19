f9e35ea213b014d43c162e8eb646d300a8d6c1eb
Loulyb <louisejanum@hotmail.com>
2024-04-19 06:09:50 +0200

    noter i js

--- opret.js
+++ opret.js
@@ -9,6 +9,7 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+// Hovedfunktionen, der styrer visningen af slides.
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("myfade");
@@ -16,7 +17,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none"; // Skjuler slides
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
@@ -25,5 +26,5 @@ function showSlides(n) {
     slides[i].classList.remove("active"); // Fjern aktiv klassen fra alle slides
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " active"; // Marker det aktuelle punkt
 }
